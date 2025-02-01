a0303a8ac9e8925ce745c851a9a1dc39aebafa5e
ihaveahax <ian@ianburgwin.net>
2025-02-01 17:43:54 -0600

    common-nixos: add nixpkgs-config.nix etc file

--- common-nixos/cfg-nix-settings.nix
+++ common-nixos/cfg-nix-settings.nix
@@ -3,10 +3,19 @@
   lib,
   pkgs,
   me,
+  inputs,
   ...
 }:
 
+let
+  nixpkgs-config-local = import ../extras/nixpkgs-config-local.nix { inherit pkgs inputs; };
+in
 {
+  # seems nixpkgs already sets NIXPKGS_CONFIG to this path
+  environment = {
+    etc."nix/nixpkgs-config.nix".text = nixpkgs-config-local;
+  };
+
   nix.settings = {
     # needed for cachix and linux-builder
     # root is assumed to be here
---
+++ extras/nixpkgs-config-local.nix
@@ -0,0 +1,13 @@
+{ pkgs, inputs }:
+
+# this is to be set as a NIXPKGS_CONFIG env var
+# but this should also go in /etc or something
+''
+{
+  allowUnfree = true;
+
+  packageOverrides = pkgs: {
+    hax = import ${inputs.hax-nur} { inherit pkgs; };
+  };
+}
+''
