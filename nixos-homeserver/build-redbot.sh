394bba995962f28304228fbd9a5cc8a3fab057e4
ihaveahax <ian@ianburgwin.net>
2025-02-07 11:26:52 -0600

    nixos-tataru: update redbot to 3.5.16

--- nixos-homeserver/build-redbot.sh
+++ nixos-homeserver/build-redbot.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-export VERSION="3.5.13"
+export VERSION="3.5.16"
 export IMAGE="ianburgwin/red-discordbot"
+export BASEIMAGE="amd64/python:3.11-slim"
 
-docker build --build-arg VERSION=$VERSION --platform linux/amd64 -t $IMAGE - < redbot.dockerfile
+docker pull $BASEIMAGE
+docker build --build-arg VERSION=$VERSION --build-arg BASEIMAGE=$BASEIMAGE --platform linux/amd64 -t $IMAGE - < redbot.dockerfile
 docker tag $IMAGE $IMAGE:$VERSION
 docker push $IMAGE
 docker push $IMAGE:$VERSION
