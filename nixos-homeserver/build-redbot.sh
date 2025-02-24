0b4fbb547984299ac30af08bf0bf531cb7910e93
ihaveahax <ian@ianburgwin.net>
2025-02-24 17:14:20 -0600

    nixos-homeserver: put redbot info into json file

--- nixos-homeserver/build-redbot.sh
+++ nixos-homeserver/build-redbot.sh
@@ -1,11 +1,18 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-export VERSION="3.5.16"
-export IMAGE="ianburgwin/red-discordbot"
-export BASEIMAGE="amd64/python:3.11-slim"
+set -xe
+
+curdir=$(dirname "$0")
+infofile=$curdir/redbot-info.json
+
+echo "Info file: $infofile"
+
+export VERSION=$(jq -r .version < "$infofile")
+export IMAGE=$(jq -r .imagename < "$infofile")
+export BASEIMAGE=$(jq -r .baseimage < "$infofile")
 
 docker pull $BASEIMAGE
-docker build --build-arg VERSION=$VERSION --build-arg BASEIMAGE=$BASEIMAGE --platform linux/amd64 -t $IMAGE - < redbot.dockerfile
+docker build --build-arg VERSION=$VERSION --build-arg BASEIMAGE=$BASEIMAGE --platform linux/amd64 -t $IMAGE - < "$curdir/redbot.dockerfile"
 docker tag $IMAGE $IMAGE:$VERSION
 docker push $IMAGE
 docker push $IMAGE:$VERSION
