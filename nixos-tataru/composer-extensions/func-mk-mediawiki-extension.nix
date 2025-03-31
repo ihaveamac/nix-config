ce616b874aca47e238e6c5756b56a74f6ae3aaca
ihaveahax <ian@ianburgwin.net>
2025-03-31 00:37:06 -0500

    nixos-tataru: fix mw extension wrapper

--- nixos-tataru/composer-extensions/func-mk-mediawiki-extension.nix
+++ nixos-tataru/composer-extensions/func-mk-mediawiki-extension.nix
@@ -22,7 +22,7 @@ let
         writeShellScript "wrap-composer-package" ''
           # this will break if an extension includes a folder called "share"...
           linkRepoFiles () {
-            find -L $out/share/php/$pname -maxdepth 1 -print0 | while read -d $'\0' f; do
+            find -L $out/share/php/$pname -maxdepth 1 -mindepth 1 -print0 | while read -d $'\0' f; do
               ln -s "$f" $out/$(basename "$f")
             done
           }
